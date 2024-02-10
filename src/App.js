d936370e29466e342661358ae47bdb4e6df2246c
Sarvesh Reshimwale <92729924+Sarvesh0827@users.noreply.github.com>
2024-02-10 08:41:30 +0530

    https://github.com/Sarvesh0827/Mern1.git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from "@mui/material/Container";
+import AppBar from "@mui/material/AppBar";
+import Typography from "@mui/material/Typography";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 
-function App() {
+import memories from "../src/images/memories.png";
+import { Grid, Grow } from "@mui/material";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container maxWidth="lg">
+        <AppBar position="static" color="inherit">
+          <Typography variant="h5" align="center">
+            Memories
+          </Typography>
+          <img src={memories} alt="memories" height={60}/>
+
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid
+              container
+              justifyContent="space-between"
+              alignItems="stretch"
+              spacing={3}
+            >
+              <Grid item xs={12} sm={7}>
+                <Posts/>
+                </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form />
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
